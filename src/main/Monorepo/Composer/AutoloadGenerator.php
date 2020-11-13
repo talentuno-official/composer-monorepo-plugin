0bd9887ddda8be5a6af7b520734929df761380c9
Kalman Peter <peter.kalman@gwshub.com>
2020-11-13 13:12:19 +0100

    laminas/laminas-zendframework-bridge conflict

--- src/main/Monorepo/Composer/AutoloadGenerator.php
+++ src/main/Monorepo/Composer/AutoloadGenerator.php
@@ -15,32 +15,4 @@ class AutoloadGenerator extends \Composer\Autoload\AutoloadGenerator
 
         return $packageMap;
     }
-
-    protected function getAutoloadRealFile($useClassMap, $useIncludePath, $targetDirLoader, $useIncludeFiles, $vendorPathCode, $appBaseDirCode, $suffix, $useGlobalIncludePath, $prependAutoloader, $staticPhpVersion = 70000, $checkPlatform)
-    {
-        $file = parent::getAutoloadRealFile($useClassMap, $useIncludePath, $targetDirLoader, false, $vendorPathCode, $appBaseDirCode, $suffix, $useGlobalIncludePath, $prependAutoloader, $staticPhpVersion, $checkPlatform);
-
-        if (! $useIncludeFiles) {
-            return $file;
-        }
-
-        return $file .= <<<INCLUDE_FILES
-
-\$includeFiles = require __DIR__ . '/autoload_files.php';
-foreach (\$includeFiles as \$file) {
-    composerRequireOnce$suffix(\$file);
-}
-
-function composerRequireOnce$suffix(\$file)
-{
-    require_once \$file;
-}
-
-INCLUDE_FILES;
-    }
-
-    protected function filterPackageMap(array $packageMap, PackageInterface $mainPackage)
-    {
-        return $packageMap;
-    }
 }
